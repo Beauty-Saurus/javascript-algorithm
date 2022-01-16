2c09797b9e953a9bac01b450602811c94ec05550
Chloekkk <skdusdl8804@gmail.com>
2022-01-17 00:23:33 +0900

    :sparkles: complete: 숫자 문자열과 영단어

---
+++ nkim/week4/숫자 문자열과 영단어.js
@@ -0,0 +1,16 @@
+function solution(s) {
+  var answer = "";
+  answer = s
+    .replace(/zero/g, "0")
+    .replace(/one/g, "1")
+    .replace(/two/g, "2")
+    .replace(/three/g, "3")
+    .replace(/four/g, "4")
+    .replace(/five/g, "5")
+    .replace(/six/g, "6")
+    .replace(/seven/g, "7")
+    .replace(/eight/g, "8")
+    .replace(/nine/g, "9");
+  answer = parseInt(answer);
+  return answer;
+}
