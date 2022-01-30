dbfd8fab0a9f96f9bd6d45226fb060070d30daf1
Chloekkk <skdusdl8804@gmail.com>
2022-01-30 17:10:38 +0900

    complete : week6

---
+++ nkim/week6/실패율.js
@@ -0,0 +1,20 @@
+function solution(N, stages) {
+    var answer = [];
+    const report = {};
+    [...new Array(N)].forEach((_, i) => {
+        report[i + 1] = {stop: 0, dare: 0, fail_rate: 0};
+    });
+
+	Object.keys(report).forEach((stage) => {
+		report[stage].stop = stages.filter(s => s == stage).length;
+		report[stage].dare = stages.filter(s => s >= stage).length;
+	});
+	Object.entries(report).forEach(([stage, val]) => {
+		const {stop, dare} = val;
+		report[stage].fail_rate = stop / dare;
+	});
+	answer = Object.keys(report).sort((i, j) =>
+                    report[j].fail_rate - report[i].fail_rate);
+    answer = answer.map(data => parseInt(data));
+    return answer;
+}
